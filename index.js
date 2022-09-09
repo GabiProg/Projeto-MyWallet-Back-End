2683e73a1d55e135f05173dd2e99d4b9a0303c4b
GabiProg <gabrielaterese07@gmail.com>
2022-09-09 16:20:53 -0300

    POST das rotas / e /sign-up feitos

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import joi from 'joi';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
+
 dotenv.config();
 
 const app = express();
@@ -13,6 +17,95 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 mongoClient.connect().then(() => {
     db = mongoClient.db('mywallet');
-}); 
+});
+
+app.post('/sign-up', async (req, res) => {
+    const user = req.body;
+    const { password, confirmation } = req.body;
+    
+    const userSchema = joi.object({
+        name: joi.string().required(),
+        email: joi.string().email().required(),
+        password: joi.string().required().min(6),
+        confirmation: joi.string().required().min(6)
+    });
+
+    const validation = userSchema.validate(user, {abortEarly: false});
+
+    if(validation.error){
+        const erros = validation.error.details.map(detail => detail.message);
+        res.status(422).send(erros);
+        return;
+    }
+
+    if(password !== confirmation){
+        res.sendStatus(409);
+        return;
+    }
+
+    try {
+        const alreadyAuser = await db.collection('cadastros').findOne(user);
+        if(alreadyAuser){
+            res.sendStatus(409);
+            return;
+        }
+
+        const cryptoPassword = bcrypt.hashSync(password, 10);
+        const cryptoPasswordConfirmation = bcrypt.hashSync(confirmation, 10);
+
+        await db.collection('cadastros').insertOne({...user, password: cryptoPassword, confirmation: cryptoPasswordConfirmation});
+
+        res.status(201).send('Usuário cadastrado.');
+
+    } catch (err) {
+        res.sendStatus(500);
+    }
+});
+
+app.post('/', async (req, res) => {
+    const { email, password } = req.body;
+
+    const userSchema = joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().required().min(6)
+    });
+
+    const validation = userSchema.validate(req.body, {abortEarly: false});
+
+    if(validation.error){
+        const erros = validation.error.details.map(detail => detail.message);
+        res.status(422).send(erros);
+        return;
+    }
+
+    try {
+        const findUser = await db.collection('cadastros').findOne({email: email});
+
+        if(findUser && bcrypt.compareSync(password, findUser.password)){
+
+            const token = uuid();
+
+            await db.collection('sessoes').insertOne({
+                token,
+                userId: findUser._id
+            });
+
+            res.status(201).send({token});
+
+            return;
+        } else {
+            res.status(401).send('Senha ou e-mail incorretos.');
+            return;
+        }
+
+        
+
+        
+
+    } catch (err){
+    res.sendStatus(500);
+    }   
+
+});
 
 app.listen(5000, () => console.log('Listening on port 5000.'));
